281dbe04db45a0826ae57f2315303d231ed24bc7
Diego Salgado <dasalgadob@unal.edu.co>
2019-05-28 11:52:06 -0500

    Closes #38 Company and business unit strings fixed

--- app/helpers/areas_helper.rb
+++ app/helpers/areas_helper.rb
@@ -1,5 +1,5 @@
 module AreasHelper
   def get_area_business_unit_string(area)
-    return area.business_unit == nil ? area.company.name : area.business_unit.name
+    return area.business_unit == nil ? "" : area.business_unit.name
   end
 end
